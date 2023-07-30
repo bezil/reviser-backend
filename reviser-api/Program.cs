540c7d16d2791efed6382a50fba7b9ca185beed2
Basil <basil.george@aspireapp.com>
2023-07-30 23:29:10 +0530

    cors

--- reviser-api/Program.cs
+++ reviser-api/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options => options.AddPolicy("All",
+    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,7 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("All");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
